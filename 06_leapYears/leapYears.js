03df2f8ef115aeb101c2198852e1135af530dbce
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-24 11:42:05 -0700

    Add solution to exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divBy400 = year % 400 == 0;
+    const divBy100 = year % 100 == 0;
+    const divBy4 = year % 4 == 0;
+    return divBy400 || (divBy4 && !divBy100);
 };
 
 // Do not edit below this line
