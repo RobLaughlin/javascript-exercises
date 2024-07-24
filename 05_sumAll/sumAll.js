ea2911aabd41b812c190318986c0461d5872eba5
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-24 11:36:40 -0700

    Add solution to exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (
+        typeof(a) !== "number" || typeof(b) !== "number" 
+        || a < 0 || b < 0
+        || !Number.isInteger(a) || !Number.isInteger(b)
+    ) { return "ERROR"; }
 
+    const m = Math.max(a, b);
+    const n = Math.min(a, b);
+
+    // Use formula 1 + 2 + 3 + ... + n = n(n+1)/2
+    const sum = m*(m+1)/2
+    const sub = n*(n+1)/2
+
+    return sum - sub + n;
 };
 
 // Do not edit below this line
