f9224aaa50e515d63613d228b0927fb8e86c7703
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-24 11:09:07 -0700

    Add solution to exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
+    for (let i = str.length; i--; i >= 0) {
+        reversed += str[i];
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
