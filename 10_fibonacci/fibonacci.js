013461af106915f5c685d5e28dc00f6b1c213f9f
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 12:09:52 -0700

    Add solution to exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n === 0) { return 0; }
+    if (n < 0) { return "OOPS"; }
+    
+    let fibSeq = [1,1];
+    while (fibSeq.length < n) {
+        const end = fibSeq.length - 1;
+        fibSeq.push(fibSeq[end] + fibSeq[end - 1]);
+    }
+    return fibSeq[fibSeq.length - 1];
 };
 
 // Do not edit below this line
