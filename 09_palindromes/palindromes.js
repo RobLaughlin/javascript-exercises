6341507475ea76133592e1370da2aecd0b153331
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 11:57:47 -0700

    Add solution for exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    // Remove any character that is not a letter or number.
+    str = str.replaceAll(/[^a-zA-Z0-9]/g, "");
+    
+    let l = 0;
+    let r = str.length - 1;
+    while (l <= r) {
+        if (str[l].toLowerCase() != str[r].toLowerCase()) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
