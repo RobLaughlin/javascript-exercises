3ae446567420977cf457213754e0606ee8b4d363
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 12:28:36 -0700

    Add solution to exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleAge = people.map(person => {
+        let age = new Date().getFullYear() - person.yearOfBirth;
+        if ("yearOfDeath" in person) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return {
+            name: person.name,
+            age: age
+        }
+    });
 
+    peopleAge.sort((p1, p2) => 
+        (p1.age - p2.age)
+    );
+    
+    return peopleAge[peopleAge.length - 1];
 };
 
 // Do not edit below this line
